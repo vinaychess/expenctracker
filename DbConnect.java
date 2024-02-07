481611b41bb7d9a95e91a2c110bc0b67df5cfefd
vinaychess <111564285+vinaychess@users.noreply.github.com>
2024-02-07 21:15:02 +0530

    Add files via upload

---
+++ DbConnect.java
@@ -0,0 +1,17 @@
+package db;
+import java.sql.*;
+import javax.swing.JOptionPane;
+public class DbConnect {
+    public static Connection c;
+    public static Statement st;
+    static{
+        try{
+            c=DriverManager.getConnection(
+      "jdbc:mysql://localhost:3306/spendingdb"+"?useSSL=false",
+                 "root", "Incapp@12");
+            st=c.createStatement();
+        }catch(Exception ex){
+            JOptionPane.showMessageDialog(null, ex);
+        }
+    }
+}
